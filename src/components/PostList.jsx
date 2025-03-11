a8783cd439dd284d703a0584024f19ac13af9e79
DiegoMayorga <diegoamayorgat@gmail.com>
2025-03-11 10:20:38 -0500

    feat: adding the NewPost component and its styles and using the useState hook to update the card content through lifting state up.

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+import NewPost from "./NewPost";
 import Post from "./Post";
-import classes from "./PostList.module.css"
+import classes from "./PostList.module.css";
 
 export default function PostList() {
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+  function changeBodyHandler(e) {
+    setEnteredBody(e.target.value);
+  }
+
+  function changeAuthorHandler(e) {
+    setEnteredAuthor(e.target.value);
+  }
+
   return (
-    <ul className={classes.posts}>
-      <Post author="Diego" body="React.js is awesome!" />
-      <Post author="Alejandro" body="TypeScript is incredible!" />
-    </ul>
+    <>
+      <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler}/>
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Alejandro" body="TypeScript is incredible!" />
+      </ul>
+    </>
   );
 }
