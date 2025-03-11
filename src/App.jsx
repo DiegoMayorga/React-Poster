344b209108d64ac43ae97204f3ef2498e83c214f
DiegoMayorga <diegoamayorgat@gmail.com>
2025-03-11 11:05:03 -0500

    feat: new MainHeader component and its modular css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import MainHeader from "./components/MainHeader";
 import PostList from "./components/PostList";
 
 function App() {
   return (
     <main>
+      <MainHeader />
       <PostList />
     </main>
   );
