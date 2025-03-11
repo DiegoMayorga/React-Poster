f8191b7aa755e71b6071c7455893527b1bead69b
DiegoMayorga <diegoamayorgat@gmail.com>
2025-03-11 14:23:38 -0500

    fix: avoiding mistakes while the content is displayed.

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,21 +2,22 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 import Modal from "./Modal";
+import { useState } from "react";
 
 export default function PostList({ isPosting, onClose }) {
-  let modalContent;
+  const [/* posts,  */setPosts] = useState([]);
 
-  if (isPosting) {
-    modalContent = (
-      <Modal onClose={onClose}>
-        <NewPost onCancel={onClose} />
-      </Modal>
-    );
+  function addPostHandler(postData) {
+    setPosts((prevPosts) => [postData, ...prevPosts]);
   }
 
   return (
     <>
-      {modalContent}
+      {isPosting && (
+        <Modal onClose={onClose}>
+          <NewPost onCancel={onClose} onAddPost={addPostHandler} />
+        </Modal>
+      )}
       <ul className={classes.posts}>
         <Post author="Alejandro" body="TypeScript is incredible!" />
       </ul>
