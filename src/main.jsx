6a463a714e43c92fab3131f12f1f847ead7dc1c0
DiegoMayorga <diegoamayorgat@gmail.com>
2025-03-12 10:49:37 -0500

    feat: using React router to handle the routes keeping the app as SPA.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+
+import RootLayout from "./routes/Root";
+import App from "./App";
+import NewPost from "./routes/NewPost";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <App /> },
+      { path: "create-post", element: <NewPost /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
