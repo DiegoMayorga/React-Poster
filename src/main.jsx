901349a7b514b7c5d2b10aec037a21c0be21919f
DiegoMayorga <diegoamayorgat@gmail.com>
2025-03-12 11:59:36 -0500

    refactor: using loader to fetch data.

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
 import RootLayout from "./routes/Root";
-import Posts from "./routes/Posts";
+import Posts, { loader as postsLoader } from "./routes/Posts";
 import NewPost from "./routes/NewPost";
 
 const router = createBrowserRouter([
@@ -15,6 +15,7 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Posts />,
+        loader: postsLoader,
         children: [{ path: "/create-post", element: <NewPost /> }],
       },
     ],
